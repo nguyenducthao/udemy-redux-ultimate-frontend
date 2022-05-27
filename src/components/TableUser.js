a1e8091e379e5e54a909f3dd43ace7ec17592fb7
nguyenducthao <ndthaodng@gmail.com>
2022-05-27 16:42:44 +0700

    21 Loading Status - Trang Thai Fetch APIs

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,13 +1,14 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { Container, Table } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchAllUsers } from "../action/actions"
 
 const TableUser = (props) => {
     // const [listUsers, setListUsers] = useState([])
-    const listUsers = useSelector((state) => state.user.listUsers)
     const dispatch = useDispatch()
+    const listUsers = useSelector((state) => state.user.listUsers)
+    const isLoading = useSelector((state) => state.user.isLoading)
+    const isError = useSelector((state) => state.user.isError)
     useEffect(() => {
         dispatch(fetchAllUsers())
     }, [])
@@ -27,18 +28,35 @@ const TableUser = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
-                        return (
-                            <tr key={`users-${index}`}>
-                                <td>{index + 1}</td>
-                                <td>{item.email}</td>
-                                <td>{item.username}</td>
-                                <td>
-                                    <button className="btn btn-danger" onClick={() => handleOnDeleteUser(item)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    })}
+                    {isError ?
+                        <>
+                            <div>Something wrong, please try again...</div>
+                        </>
+                        :
+                        <>
+                            {isLoading ?
+                                <>
+                                    <tr>
+                                        <td colSpan={4}>Loading</td>
+                                    </tr>
+                                </>
+                                :
+                                <>
+                                    {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
+                                        return (
+                                            <tr key={`users-${index}`}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.email}</td>
+                                                <td>{item.username}</td>
+                                                <td>
+                                                    <button className="btn btn-danger" onClick={() => handleOnDeleteUser(item)}>Delete</button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </>}
+                        </>
+                    }
                 </tbody>
             </Table>
         </Container>
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,22 +1,24 @@
 import { FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_ERROR } from '../action/types';
 
 const INITIAL_STATE = {
-    listUsers: []
+    listUsers: [],
+    isLoading: false,
+    isError: false
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case FETCH_USER_REQUEST:
             return {
-                ...state
+                ...state, isLoading: true, isError: false
             };
         case FETCH_USER_SUCCESS:
             return {
-                ...state, listUsers: action.dataUsers
+                ...state, listUsers: action.dataUsers, isLoading: false, isError: false
             };
         case FETCH_USER_ERROR:
             return {
-                ...state
+                ...state, isLoading: false, isError: true
             };
         default: return state;
     }
